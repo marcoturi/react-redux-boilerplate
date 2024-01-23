91c5dc5abb844f8be4838d4c22dc7f59e82b8cdb
Marco <marco.turi@hotmail.it>
2024-01-24 00:30:24 +0100

    refactor(UI): create skeleton list

--- src/UI/Elements/Skeleton/Skeleton.tsx
+++ src/UI/Elements/Skeleton/Skeleton.tsx
@@ -12,4 +12,32 @@ function Skeleton({
   );
 }
 
-export { Skeleton };
+function SkeletonCard({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn('flex items-center space-x-4', className)}
+      {...props}
+    >
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+  );
+}
+
+function SkeletonList({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div {...props}>
+      <SkeletonCard className="py-5" />
+      <SkeletonCard className="py-5" />
+      <SkeletonCard className="py-5" />
+    </div>
+  );
+}
+
+export { Skeleton, SkeletonCard, SkeletonList };
--- src/features/subscriptions/components/SubscriptionList.tsx
+++ src/features/subscriptions/components/SubscriptionList.tsx
@@ -1,5 +1,5 @@
 import { SubscriptionApi, SubscriptionSelectors } from '../store';
-import { Skeleton } from '@/UI/Elements/Skeleton/Skeleton';
+import { SkeletonList } from '@/UI/Elements/Skeleton/Skeleton';
 import { useAppSelector } from '@/core/store/types';
 import { SubscriptionCard } from '@/features/subscriptions/components';
 
@@ -10,15 +10,7 @@ export function SubscriptionList() {
   );
 
   if (isLoading) {
-    return (
-      <div className="flex items-center space-x-4">
-        <Skeleton className="h-12 w-12 rounded-full" />
-        <div className="space-y-2">
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[200px]" />
-        </div>
-      </div>
-    );
+    return <SkeletonList className="py-8" />;
   }
   return subscription?.map((s) => (
     <SubscriptionCard
