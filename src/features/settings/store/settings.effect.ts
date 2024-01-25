0626ee6254f931f9baa0ab2e72317fa5d814dc96
Marco <marco.turi@hotmail.it>
2024-01-25 01:28:30 +0100

    docs(Settings): add disclaimer about localstorage

--- src/features/settings/store/settings.effect.ts
+++ src/features/settings/store/settings.effect.ts
@@ -8,6 +8,12 @@ import { createListenerMiddleware, PayloadAction } from '@reduxjs/toolkit';
 
 export const storageMiddleware = createListenerMiddleware<any>();
 
+/**
+ * Important: this middleware is here only to showcase the usage of ListenerMiddleware.
+ * In a real app, if you want to persist your redux state to local storage
+ * you should use something like redux-persist instead.
+ * https://github.com/markerikson/redux-ecosystem-links/blob/master/store-persistence.md
+ */
 storageMiddleware.startListening({
   actionCreator: SettingsActions.setItem,
   effect: (action: PayloadAction<SetValuePayload>) => {
--- src/features/settings/store/settings.thunk.ts
+++ src/features/settings/store/settings.thunk.ts
@@ -3,6 +3,12 @@ import LocalStorageService, {
 } from '@/core/helpers/local-storage.service';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+/**
+ * Important: this thunk is here only to showcase the usage of a thunk.
+ * In a real app, if you want to persist your redux state to local storage
+ * you should use something like redux-persist instead.
+ * https://github.com/markerikson/redux-ecosystem-links/blob/master/store-persistence.md
+ */
 export const loadState = createAsyncThunk('settings/loadState', () =>
   LocalStorageService(StorageKeys.settings).getAll(),
 );
