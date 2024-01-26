a9ecd5ae2a7ceb0cf609329891a913f97a598613
Marco <marco.turi@hotmail.it>
2024-01-26 13:37:32 +0100

    test(settings): add slice tests

---
+++ src/features/settings/store/settings.slice.spec.ts
@@ -0,0 +1,66 @@
+import { SettingsActions } from '@/features/settings/store/index';
+import {
+  initialStorageState,
+  settingsSlice,
+} from '@/features/settings/store/settings.slice';
+import { SettingsKey } from '@/features/settings/store/settings.type';
+import { SubscriptionFilters } from '@/features/subscriptions/components/subscription.types';
+import { loadState } from '@/features/settings/store/settings.thunk';
+
+describe('Settings Slice:', () => {
+  it('Should handle setItem', () => {
+    const action = SettingsActions.setItem({
+      key: SettingsKey.filters,
+      value: {
+        filterSubscriptionsBy: 'test',
+      },
+    });
+    const expectedState = {
+      ...initialStorageState,
+      [SettingsKey.filters]: {
+        filterSubscriptionsBy: 'test',
+      },
+    };
+    expect(settingsSlice.reducer(undefined, action)).toEqual(expectedState);
+  });
+  it('Should handle deleteItem in case of matching key', () => {
+    const action = SettingsActions.deleteItem({
+      key: SettingsKey.filters,
+    });
+    const initialState = {
+      ...initialStorageState,
+      [SettingsKey.filters]: {
+        filterSubscriptionsBy: SubscriptionFilters.All,
+      },
+    };
+    const expectedState = {
+      ...initialStorageState,
+    };
+    expect(settingsSlice.reducer(initialState, action)).toEqual(expectedState);
+  });
+  it('Should handle deleteItem in case of not matching key', () => {
+    const action = SettingsActions.deleteItem({
+      key: 'unknown' as any,
+    });
+    const initialState = {
+      ...initialStorageState,
+      [SettingsKey.filters]: {
+        filterSubscriptionsBy: SubscriptionFilters.All,
+      },
+    };
+    expect(settingsSlice.reducer(initialState, action)).toEqual(initialState);
+  });
+  it('Should handle loadState.fulfilled', () => {
+    const thunkResult = {
+      [SettingsKey.filters]: {
+        filterSubscriptionsBy: SubscriptionFilters.All,
+      },
+    };
+    const action = (loadState as any).fulfilled(thunkResult);
+    const expectedState = {
+      ...initialStorageState,
+      ...thunkResult,
+    };
+    expect(settingsSlice.reducer(initialStorageState, action)).toEqual(expectedState);
+  });
+});
