4cbf231fba2eebc700ebf13b0fc85d46d75811f2
Marco <marco.turi@hotmail.it>
2024-01-28 23:55:16 +0100

    chore(App): enhance testing capabilities and filter tests in coverage

--- scripts/vitest.setup.ts
+++ scripts/vitest.setup.ts
@@ -4,6 +4,14 @@ import * as matchers from "@testing-library/jest-dom/matchers";
 import { setupServer } from 'msw/node';
 
 expect.extend(matchers);
+declare var window;
+
+// Needed for testing Radix Select component
+// https://github.com/testing-library/user-event/discussions/1087
+window.PointerEvent = class PointerEvent extends Event {};
+window.HTMLElement.prototype.scrollIntoView = vi.fn();
+window.HTMLElement.prototype.hasPointerCapture = vi.fn();
+window.HTMLElement.prototype.releasePointerCapture = vi.fn();
 
 export const server = setupServer(...handlers);
 
